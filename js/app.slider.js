0adaa9917d9b6183985c9a8c95c2c44cd894a6d5
Brian Wynn <b_wynn@apple.com>
2015-08-14 14:02:17 -0700

    Copy change event working as expected within the slider module.

--- js/app.slider.js
+++ js/app.slider.js
@@ -4,31 +4,39 @@ var
   configMap = String() +
           '<div id="slider">' +
             '<h1>Slider module now initialized</h1>' +
+            '<a href="#" class="btn">Change content</a>' +
           '</div>',
   stateMap = {
     $append_target: null
   },
   jqueryMap = {},
 
-setJqueryMap, changeText, initModule;
+setJqueryMap, newText, changeText, initModule;
 // ------------------- END MODULE SCOPE VARIABLES ------------------------------
 
 // ------------------- DOM METHODS ---------------------------------------------
 setJqueryMap = function() {
   var $append_target = stateMap.$append_target,
-  $slider = $append_target.find( '#slider' );
+      $slider = $append_target.find( '#slider' );
+
   jqueryMap = {
     $slider : $slider,
-    $h1: $slider.find('h1')
+    $h1: $slider.find('h1'),
+    $btn: $slider.find('a.btn')
    };
 };
 
-changeText = function() {
+newText = function() {
   jqueryMap.$h1.text("New content for the slider module");
 };
 // ------------------- END DOM METHODS -----------------------------------------
 
 // ------------------- BEGIN EVENT HANDLERS ------------------------------------
+changeText = function() {
+  jqueryMap.$btn.on("click", function() {
+    return newText();
+  });
+};
 // ------------------- END EVENT HANDLERS --------------------------------------
 
 // ------------------- BEGIN PUBLIC METHODS ------------------------------------
